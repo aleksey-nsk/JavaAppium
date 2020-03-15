33b46fa75c98be6cc7fd63a189b3f8c02e858d90
Alex Zhd <my_email@domain.com>
2020-03-15 21:54:32 +0700

    Ex2: Создание метода

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
 import java.net.URL;
 
@@ -40,6 +45,65 @@ public class FirstTest {
   @Test
   public void firstTest() {
     System.out.print("\n\n***** Внутри метода firstTest() *****\n\n");
-    System.out.println("Запускаю первый тест");
+
+    waitForElementAndClick(
+        By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+        "Can not find 'Search Wikipedia' input",
+        5
+    );
+
+    WebElement searchField = waitForElementPresent(
+        By.id("org.wikipedia:id/search_src_text"),
+        "Can not find search field"
+    );
+
+    checkTextInElement(searchField, "Search…");
+
+    sendKeysInElement(searchField, "Java");
+
+    waitForElementPresent(
+        By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+        "Can not find 'Object-oriented programming language' topic, searching by 'Java'",
+        15
+    );
+  }
+
+  private WebElement waitForElementPresent(By locator, String errorMessage) {
+    return waitForElementPresent(locator, errorMessage, 5);
+  }
+
+  private WebElement waitForElementPresent(By locator, String errorMessage, long timeoutInSeconds) {
+    System.out.println("Дождаться элемент: locator = '" + locator + "', таймаут = " + timeoutInSeconds + " секунд");
+    WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+    wait.withMessage(errorMessage + "\n");
+    WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    return element;
+  }
+
+  private WebElement waitForElementAndClick(By locator, String errorMessage, long timeoutInSeconds) {
+    System.out.println("Дождаться элемент, и кликнуть по нему");
+    WebElement element = waitForElementPresent(locator, errorMessage, timeoutInSeconds);
+    System.out.println("Кликнуть по элементу");
+    element.click();
+    return element;
+  }
+
+  private WebElement waitForElementAndSendKeys(By locator, String value, String error_message, long timeoutInSeconds) {
+    System.out.println("Дождаться элемент, и ввести в него значение");
+    WebElement element = waitForElementPresent(locator, error_message, timeoutInSeconds);
+    System.out.println("Ввести в элемент значение: '" + value + "'");
+    element.sendKeys(value);
+    return element;
+  }
+
+  private void sendKeysInElement(WebElement element, String value) {
+    System.out.println("Ввести в элемент значение: '" + value + "'");
+    element.sendKeys(value);
+  }
+
+  private void checkTextInElement(WebElement element, String expectedTextInElement) {
+    System.out.println("Проверить наличие в элементе текста: '" + expectedTextInElement + "'");
+    String actualTextInElement = element.getAttribute("text");
+    Assert.assertEquals("Элемент содержит неверный текст", expectedTextInElement, actualTextInElement);
   }
 }
