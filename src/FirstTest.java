8c634974f51aee2ddade2f7d41295fbdcda8d9b0
Alex Zhd <my_email@domain.com>
2020-04-07 21:14:10 +0700

    Урок 4, часть 1. Добавили класс CoreTestCase

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,59 +1,20 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.io.File;
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
-
-  private AppiumDriver driver;
-
-  @Before
-  public void setUp() throws Exception {
-    System.out.print("\n\n***** Внутри метода setUp() *****\n\n");
-
-    final String path = new File("apks/org.wikipedia.apk").getAbsolutePath();
-    System.out.println("Путь до тестируемого приложения: " + path);
-
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-
-    capabilities.setCapability("platformName", "Android");
-    capabilities.setCapability("deviceName", "AndroidTestDevice");
-    capabilities.setCapability("platformVersion", "6.0");
-    capabilities.setCapability("automationName", "Appium");
-    capabilities.setCapability("appPackage", "org.wikipedia");
-    capabilities.setCapability("appActivity", ".main.MainActivity");
-    capabilities.setCapability("app", path);
-
-    driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-  }
-
-  @After
-  public void tearDown() {
-    System.out.print("\n\n***** Внутри метода tearDown() *****\n\n");
-
-    System.out.println("Поворачиваем экран телефона в портретный режим");
-    driver.rotate(ScreenOrientation.PORTRAIT);
-
-    System.out.println("Вызываем метод quit()");
-    driver.quit();
-  }
+public class FirstTest extends CoreTestCase {
 
   @Test
-  public void firstTest() {
-    System.out.print("\n\n***** Внутри метода firstTest() *****\n\n");
+  public void testSearch() {
+    System.out.print("\n\n***** Внутри метода testSearch() *****\n\n");
 
     waitForElementAndClick(
         By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
@@ -112,7 +73,7 @@ public class FirstTest {
     waitForElementNotPresent(
         By.xpath("//*[@resource-id='org.wikipedia:id/search_results_container']/*[@resource-id='org.wikipedia:id/search_results_list']"),
         "Элемент, содержащий список результатов поиска, всё ещё присутствует на странице",
-        10
+        15
     );
   }
 
@@ -183,8 +144,8 @@ public class FirstTest {
   }
 
   @Test
-  public void saveFirstArticleToMyList() {
-    System.out.print("\n\n***** Внутри метода saveFirstArticleToMyList() *****\n\n");
+  public void testSaveFirstArticleToMyList() {
+    System.out.print("\n\n***** Внутри метода testSaveFirstArticleToMyList() *****\n\n");
 
     waitForElementAndClick(
         By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
